9769686cbd5bf8932452a796e78c0cda73917c12
Shankar-Kb <shankar.kb95@gmail.com>
2021-03-08 00:31:40 +0530

    finalized the build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 //import { useState } from 'react';
 //import { BrowserRouter, Route, Switch, useHistory, useLocation, useParams} from 'react-router-dom';
-const dotenv = require('dotenv');
-dotenv.config();
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import DisplayMessageComp from './dislpayMessage';
 
 function App() {
-    //const [posts, setPosts] = useState([]);
+    //const { push } = useHistory();
+    //const [text, setText] = useState("");
     //const serverURL = "https://secret-message-api.herokuapp.com";
-    const serverURL = "https://localhost:3001";
-    const clientURL = "https://localhost:3000";
+    const serverURL = "http://localhost:3001";
+    const clientURL = "http://localhost:3000/message";
 
     const sendMessage = () => {
         let data = {
@@ -17,8 +18,6 @@ function App() {
         targetURL: clientURL,
         targetMail: document.getElementById("targetMail").value
         }
-        console.log(serverURL);
-        console.log(clientURL);
         fetch(`${serverURL}/create-message`, {
               method: "POST",
               headers: {"Content-Type": "application/json; charset=UTF-8"},
@@ -30,13 +29,41 @@ function App() {
                 document.getElementById("sendMessageForm").reset();
               });  
     }
+
+    const deleteMessage = () => {
+
+      let data = {
+        secretKey: document.getElementById("randomKeyDelete").value,
+        password: document.getElementById("passwordDelete").value
+      }
+      fetch(`${serverURL}/delete-message`, { 
+        method: "DELETE",
+        headers: {"Content-Type": "application/json; charset=UTF-8"},
+        body: JSON.stringify(data) })
+         .then( resp => resp.json() )
+         .then( data => { //console.log(data);
+         console.log(data);
+         document.getElementById("deleteMessageForm").reset();
+         });
+         
+    }
+
     const generateRandomString = () => {
       const length = 6;
-      document.getElementById("randomKey").value = Math.random().toString(20).substr(2, length).toUpperCase();
+      const randomString = Math.random().toString(20).substr(2, length).toUpperCase();
+      document.getElementById("randomKey").value = randomString;
+      document.getElementById("randomKeyDelete").value = randomString;
       }      
 
   return (
-    <div className="App">
+    <BrowserRouter>
+    <div className="container-fluid">
+    <div className="row">
+    <Switch>
+    
+    <Route path='/' exact={true}>
+    <div className="send-message-box message-box col-6">
+     <h2 className="heading text-center">Send Message</h2>
      <form id="sendMessageForm">
      <div className="input-group">
      <span className="input-group-text">Secret Message</span>
@@ -51,7 +78,28 @@ function App() {
      </div>
      </form>
      <button type="button" className="btn btn-secondary" id="sendMessageButton" onClick={sendMessage}>Send Message</button>
+     </div>
+
+    <div className="delete-message-box message-box col-6">
+    <h2 className="heading text-center">Delete Message</h2>
+     <form id="deleteMessageForm">
+     <div className="input-group">
+     <span className="input-group-text">Password</span>
+     <input className="form-control" id="passwordDelete" type="password" defaultValue="" />
+     <span className="input-group-text">Random Key</span>
+     <input className="form-control" id="randomKeyDelete" type="text" defaultValue=""/>
+     </div>
+     </form>
+     <button type="button" className="btn btn-secondary" id="deleteMessageButton" onClick={deleteMessage}>Delete Message</button>
     </div>
+    </Route>
+
+    <Route path="/message/:id" render={(props) => <DisplayMessageComp {...props} serverURL={serverURL} />} />
+
+    </Switch>
+    </div>
+    </div>
+    </BrowserRouter>
   );
 }
 
